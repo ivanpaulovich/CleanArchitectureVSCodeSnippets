c7dd5bf2f7f5d8bb8f07ea8e963b579385d47565
Ivan Paulovich <ivan@paulovich.net>
2020-06-12 23:37:17 +0200

    Review

--- out/extension.js
+++ out/extension.js
@@ -1,25 +1,128 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
-const vscode_1 = require("vscode");
-const entity_generator_1 = require("./entity-generator");
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const vscode = require("vscode");
+const path = require("path");
+const fs = require("fs");
+const os = require("os");
+const parentfinder = require('find-parent-dir');
+const findupglob = require('find-up-glob');
 function activate(context) {
-    const workspaceRoot = "src";
-    const generator = new entity_generator_1.DuckGenerator(workspaceRoot, vscode_1.window);
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode_1.commands.registerCommand('extension.generateDuck', () => {
-        // The code you place here will be executed every time your command is executed
-        generator.execute();
-    });
-    context.subscriptions.push(disposable);
-    context.subscriptions.push(generator);
+    context.subscriptions.push(vscode.commands.registerCommand('extension.newEntity', newEntity));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.newEntityInterface', newEntityInterface));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.newFactory', newFactory));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.newService', newService));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.newValueObject', newValueObject));
 }
 exports.activate = activate;
-// this method is called when your extension is deactivated
+function newEntity(args) {
+    promptAndSave(args, 'entity', 'domain/entity.tmpl');
+}
+function newEntityInterface(args) {
+    promptAndSave(args, 'entity interface', 'domain/entity-interface.tmpl');
+}
+function newFactory(args) {
+    promptAndSave(args, 'factory', 'domain/factory.tmpl');
+}
+function newService(args) {
+    promptAndSave(args, 'service', 'domain/service.tmpl');
+}
+function newValueObject(args) {
+    promptAndSave(args, 'service', 'domain/value-object.tmpl');
+}
+function promptAndSave(args, type, templatetype) {
+    if (args == null) {
+        args = { _fsPath: vscode.workspace.rootPath };
+    }
+    let incomingpath = args._fsPath;
+    vscode.window.showInputBox({ ignoreFocusOut: true, prompt: 'Please enter filename', value: 'new' + type + '.cs' })
+        .then((newfilename) => {
+        if (typeof newfilename === 'undefined') {
+            return;
+        }
+        var newfilepath = incomingpath + path.sep + newfilename;
+        if (fs.existsSync(newfilepath)) {
+            vscode.window.showErrorMessage("File already exists");
+            return;
+        }
+        newfilepath = correctExtension(newfilepath);
+        var originalfilepath = newfilepath;
+        var projectrootdir = getProjectRootDirOfFilePath(newfilepath);
+        if (projectrootdir == null) {
+            vscode.window.showErrorMessage("Unable to find project.json or *.csproj");
+            return;
+        }
+        projectrootdir = removeTrailingSeparator(projectrootdir);
+        var newroot = projectrootdir.substr(projectrootdir.lastIndexOf(path.sep) + 1);
+        var filenamechildpath = newfilepath.substring(newfilepath.lastIndexOf(newroot));
+        var pathSepRegEx = /\//g;
+        if (os.platform() === "win32")
+            pathSepRegEx = /\\/g;
+        var namespace = path.dirname(filenamechildpath);
+        namespace = namespace.replace(pathSepRegEx, '.');
+        namespace = namespace.replace(/\s+/g, "_");
+        namespace = namespace.replace(/-/g, "_");
+        newfilepath = path.basename(newfilepath, '.cs');
+        openTemplateAndSaveNewFile(templatetype, namespace, newfilepath, originalfilepath);
+    });
+}
+function correctExtension(filename) {
+    if (path.extname(filename) !== '.cs') {
+        if (filename.endsWith('.')) {
+            filename = filename + 'cs';
+        }
+        else {
+            filename = filename + '.cs';
+        }
+    }
+    return filename;
+}
+function removeTrailingSeparator(filepath) {
+    if (filepath[filepath.length - 1] === path.sep) {
+        filepath = filepath.substr(0, filepath.length - 1);
+    }
+    return filepath;
+}
+function getProjectRootDirOfFilePath(filepath) {
+    var projectrootdir = parentfinder.sync(path.dirname(filepath), 'project.json');
+    if (projectrootdir == null) {
+        var csprojfiles = findupglob.sync('*.csproj', { cwd: path.dirname(filepath) });
+        if (csprojfiles == null) {
+            return null;
+        }
+        projectrootdir = path.dirname(csprojfiles[0]);
+    }
+    return projectrootdir;
+}
+function openTemplateAndSaveNewFile(templatefileName, namespace, filename, originalfilepath) {
+    vscode.workspace.openTextDocument(vscode.extensions.getExtension('ivanpaulovich.clean-architecture-csharp-snippets').extensionPath + '/src/templates/' + templatefileName)
+        .then((doc) => {
+        let text = doc.getText();
+        text = text.replace('${namespace}', namespace);
+        text = text.replace('${classname}', filename);
+        text = text.replace('${interfacename}', filename);
+        text = text.replace('${structname}', filename);
+        let cursorPosition = findCursorInTemplate(text);
+        text = text.replace('${cursor}', '');
+        fs.writeFileSync(originalfilepath, text);
+        openDocument(originalfilepath, cursorPosition);
+    });
+}
+function openDocument(originalfilepath, cursorPosition) {
+    vscode.workspace.openTextDocument(originalfilepath).then((doc) => {
+        vscode.window.showTextDocument(doc).then((editor) => {
+            let newselection = new vscode.Selection(cursorPosition, cursorPosition);
+            editor.selection = newselection;
+        });
+    });
+}
+function findCursorInTemplate(text) {
+    let cursorPos = text.indexOf('${cursor}');
+    let preCursor = text.substr(0, cursorPos);
+    let lineNum = preCursor.match(/\n/gi).length;
+    let charNum = preCursor.substr(preCursor.lastIndexOf('\n')).length;
+    return new vscode.Position(lineNum, charNum);
+}
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,153 @@
-import { commands, window, ExtensionContext } from 'vscode';
-import { DuckGenerator } from './entity-generator';
+import { ExtensionContext } from 'vscode';
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as os from 'os';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const parentfinder = require('find-parent-dir');
+const findupglob = require('find-up-glob');
+
 export function activate(context: ExtensionContext) {
-  const workspaceRoot: string = "src";
-  const generator = new DuckGenerator(workspaceRoot, window);
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = commands.registerCommand('extension.generateDuck', () => {
-    // The code you place here will be executed every time your command is executed
-    generator.execute();
-  });
-
-  context.subscriptions.push(disposable);
-  context.subscriptions.push(generator);
+  context.subscriptions.push(vscode.commands.registerCommand('extension.newEntity', newEntity));
+  context.subscriptions.push(vscode.commands.registerCommand('extension.newEntityInterface', newEntityInterface));
+  context.subscriptions.push(vscode.commands.registerCommand('extension.newFactory', newFactory));
+  context.subscriptions.push(vscode.commands.registerCommand('extension.newService', newService));
+  context.subscriptions.push(vscode.commands.registerCommand('extension.newValueObject', newValueObject));
 }
 
-// this method is called when your extension is deactivated
+function newEntity(args: any) {
+  promptAndSave(args, 'entity', 'domain/entity.tmpl');
+}
+
+function newEntityInterface(args: any) {
+  promptAndSave(args, 'entity interface', 'domain/entity-interface.tmpl');
+}
+
+function newFactory(args: any) {
+  promptAndSave(args, 'factory', 'domain/factory.tmpl');
+}
+
+function newService(args: any) {
+  promptAndSave(args, 'service', 'domain/service.tmpl');
+}
+
+function newValueObject(args: any) {
+  promptAndSave(args, 'service', 'domain/value-object.tmpl');
+}
+
+function promptAndSave(args: { _fsPath: any; } | null, type: string, templatetype: string) {
+  if (args == null) {
+    args = { _fsPath: vscode.workspace.rootPath }
+  }
+  let incomingpath: string = args._fsPath;
+  vscode.window.showInputBox({ ignoreFocusOut: true, prompt: 'Please enter filename', value: 'new' + type + '.cs' })
+    .then((newfilename) => {
+
+      if (typeof newfilename === 'undefined') {
+        return;
+      }
+
+      var newfilepath = incomingpath + path.sep + newfilename;
+
+      if (fs.existsSync(newfilepath)) {
+        vscode.window.showErrorMessage("File already exists");
+        return;
+      }
+
+      newfilepath = correctExtension(newfilepath);
+
+      var originalfilepath = newfilepath;
+
+      var projectrootdir = getProjectRootDirOfFilePath(newfilepath);
+
+      if (projectrootdir == null) {
+        vscode.window.showErrorMessage("Unable to find project.json or *.csproj");
+        return;
+      }
+
+      projectrootdir = removeTrailingSeparator(projectrootdir);
+
+      var newroot = projectrootdir.substr(projectrootdir.lastIndexOf(path.sep) + 1);
+
+      var filenamechildpath = newfilepath.substring(newfilepath.lastIndexOf(newroot));
+
+      var pathSepRegEx = /\//g;
+      if (os.platform() === "win32")
+        pathSepRegEx = /\\/g;
+
+      var namespace = path.dirname(filenamechildpath);
+      namespace = namespace.replace(pathSepRegEx, '.');
+
+      namespace = namespace.replace(/\s+/g, "_");
+      namespace = namespace.replace(/-/g, "_");
+
+      newfilepath = path.basename(newfilepath, '.cs');
+
+      openTemplateAndSaveNewFile(templatetype, namespace, newfilepath, originalfilepath);
+    });
+}
+
+function correctExtension(filename: string) {
+  if (path.extname(filename) !== '.cs') {
+    if (filename.endsWith('.')) {
+      filename = filename + 'cs';
+    } else {
+      filename = filename + '.cs';
+    }
+  }
+  return filename;
+}
+
+function removeTrailingSeparator(filepath: string) {
+  if (filepath[filepath.length - 1] === path.sep) {
+    filepath = filepath!.substr(0, filepath.length - 1);
+  }
+  return filepath;
+}
+
+function getProjectRootDirOfFilePath(filepath: string) {
+  var projectrootdir = parentfinder.sync(path.dirname(filepath), 'project.json');
+  if (projectrootdir == null) {
+    var csprojfiles = findupglob.sync('*.csproj', { cwd: path.dirname(filepath) });
+    if (csprojfiles == null) {
+      return null;
+    }
+    projectrootdir = path.dirname(csprojfiles[0]);
+  }
+  return projectrootdir;
+}
+
+function openTemplateAndSaveNewFile(templatefileName: string, namespace: string, filename: string, originalfilepath: string) {
+  vscode.workspace.openTextDocument(vscode.extensions.getExtension('ivanpaulovich.clean-architecture-csharp-snippets').extensionPath + '/src/templates/' + templatefileName)
+    .then((doc: vscode.TextDocument) => {
+      let text = doc.getText();
+      text = text.replace('${namespace}', namespace);
+      text = text.replace('${classname}', filename);
+      text = text.replace('${interfacename}', filename);
+      text = text.replace('${structname}', filename);
+      text = text.replace('${structname}', filename);
+      let cursorPosition = findCursorInTemplate(text);
+      text = text.replace('${cursor}', '');
+      fs.writeFileSync(originalfilepath, text);
+
+      openDocument(originalfilepath, cursorPosition);
+    });
+}
+
+function openDocument(originalfilepath: string, cursorPosition: vscode.Position) {
+  vscode.workspace.openTextDocument(originalfilepath).then((doc) => {
+    vscode.window.showTextDocument(doc).then((editor) => {
+      let newselection = new vscode.Selection(cursorPosition, cursorPosition);
+      editor.selection = newselection;
+    });
+  });
+}
+
+function findCursorInTemplate(text: string) {
+  let cursorPos = text.indexOf('${cursor}');
+  let preCursor = text.substr(0, cursorPos);
+  let lineNum = preCursor.match(/\n/gi)!.length;
+  let charNum = preCursor.substr(preCursor.lastIndexOf('\n')).length;
+  return new vscode.Position(lineNum, charNum);
+}
 export function deactivate() { }
